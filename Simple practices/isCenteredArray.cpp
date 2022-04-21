93f250e53e8eb7602faf61f522ec1e0398a02c76
spfvtnjr <turinumugishasouvenir@gmail.com>
2022-04-20 22:06:34 -0700

    isCentered debugged;

--- Simple practices/isCenteredArray.cpp
+++ Simple practices/isCenteredArray.cpp
@@ -1,21 +1,22 @@
 	#include<iostream>
 using namespace std;
 int isCentered(int *arr,int n){
-	int mid=(n+1)/2;
-	for(int i=0; i<n;i++){
-		if(i==mid){
-			continue;
-			cout<<"I continued";
-		}
-		else if(arr[i]<=arr[mid]	){
-		return 0;	
-		}
+ if(n%2==0) return 0;
+ int front=0;
+ int back=n-1;
+ int mid=(n-1)/2;
+ cout<<"mid is:"<<arr[mid]<<endl;
+ for(int i=0; i<n/2; i++){
+ 	if(arr[front]<arr[mid] || arr[back]<arr[mid])
+ 	return 0;
+ 	front++;
+ 	back--;
 	}
 	return 1;
 }
 
 int main(){
- int arr[]={1,2,0,2,1};
+ int arr[]={1,2,6,2,1};
  int n=sizeof(arr)/sizeof(arr[0]);
  cout<<isCentered(arr,n);
 }
