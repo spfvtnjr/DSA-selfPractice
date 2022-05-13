ed13253c173e06de7450187eba1f031c67d174e9
spfvtnjr <turinumugishasouvenir@gmail.com>
2022-05-13 06:07:10 +0200

    added meaningfull variables

--- SearchingAlgolithms/interpolationWithLoop.cpp
+++ SearchingAlgolithms/interpolationWithLoop.cpp
@@ -7,21 +7,21 @@ using namespace std;
 int interpolationSearch(int arr[], int n, int x)
 {
     // Find indexes of two corners
-    int lo = 0, hi = (n - 1);
+    int low = 0, high = (n - 1);
  
     // Since array is sorted, an element present
     // in array must be in range defined by corner
-    while (lo <= hi && x >= arr[lo] && x <= arr[hi])
+    while (low <= high && x >= arr[low] && x <= arr[high])
     {
-        if (lo == hi)
+        if (low == high)
         {
-            if (arr[lo] == x) return lo;
+            if (arr[low] == x) return low;
             return -1;
         }
         // Probing the position with keeping
         // uniform distribution in mind.
-        int pos = lo + (((double)(hi - lo) /
-            (arr[hi] - arr[lo])) * (x - arr[lo]));
+        int pos = low + (((double)(high - low) /
+            (arr[high] - arr[low])) * (x - arr[low]));
  
         // Condition of target found
         if (arr[pos] == x)
@@ -29,11 +29,11 @@ int interpolationSearch(int arr[], int n, int x)
  
         // If x is larger, x is in upper part
         if (arr[pos] < x)
-            lo = pos + 1;
+            low = pos + 1;
  
-        // If x is smaller, x is in the lower part
+        // If x is smaller, x is in the lowwer part
         else
-            hi = pos - 1;
+            high = pos - 1;
     }
     return -1;
 }
@@ -41,7 +41,7 @@ int interpolationSearch(int arr[], int n, int x)
 // Driver Code
 int main()
 {
-    // Array of items on which search will
+    // Array of items on whighch search will
     // be conducted.
     int arr[] = {10, 12, 13, 16, 18, 19, 20, 21,
                  22, 23, 24, 33, 35, 42, 47};
